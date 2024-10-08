4abd469d47b2f070a4856a650029300d1a764caf
opilane <opilane@HP-CZC2349HV0>
2024-10-08 13:55:16 +0300

    Add project files.

---
+++ Controllers/ToodeController.cs
@@ -0,0 +1,59 @@
+﻿using Buivol_web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buivol_web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Toode _toode = new Toode(1, "Koola", 1.5, true);
+
+        // GET: toode
+        [HttpGet]
+        public Toode GetToode()
+        {
+            return _toode;
+        }
+
+        // GET: toode/suurenda-hinda
+        [HttpGet("suurenda-hinda")]
+        public Toode SuurendaHinda()
+        {
+            _toode.Price = _toode.Price + 1;
+            return _toode;
+        }
+
+        // GET: toode/true->false-false->true
+        [HttpGet("true->false-false->true")]
+        public Toode TrueFalse()
+        {
+            if(_toode.IsActive)
+            {
+                _toode.IsActive = false;
+            }
+            else
+            {
+                _toode.IsActive = true;
+            }
+            return _toode;
+        }
+
+        // GET: muuta-nimi
+        [HttpGet("muuta-nimi")]
+        public Toode MuutaNimi()
+        {
+            _toode.Name = _toode.Name;
+            return _toode;
+        }
+
+        // GET: muuta-hinda
+        [HttpGet("muuta-hinda")]
+        public Toode MuutaHinda()
+        {
+            _toode.Price *= _toode.Price;
+            return _toode;
+        }
+    }
+}
---
+++ Models/Kasutajad.cs
@@ -0,0 +1,11 @@
+﻿namespace Buivol_web.Models
+{
+    public class Kasutajad
+    {
+        public int Id { get; set; }
+        public string Kasutajanimi { get; set; }
+        public string Parool { get; set; }
+        public string Nimi { get; set; }
+        public string Perenimi { get; set; }
+    }
+}
