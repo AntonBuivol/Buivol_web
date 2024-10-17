9abea9a95e6c37bd679e7910557dde2cf1efdcbd
opilane <opilane@HP-CZC2349HV0>
2024-10-17 11:19:57 +0300

    pakiautomaatid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,71 +1,27 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [tooted, setTooted] = useState([]);
-    const idRef = useRef();
-    const nameRef = useRef();
-    const priceRef = useRef();
-    const isActiveRef = useRef();
+    const [pakiautomaadid, setPakiautomaadid] = useState([]);
 
     useEffect(() => {
-        fetch("https://localhost:7175/tooted")
+        fetch("https://localhost:7175/ParcelMachine")
             .then(res => res.json())
-            .then(json => setTooted(json));
+            .then(json => setPakiautomaadid(json));
     }, []);
 
-    //////////////////////////////////////////////-->
-
-    function kustuta(index) {
-        fetch("https://localhost:7175/tooted/kustuta/" + index, { "method": "DELETE" })
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-    //////////////////////////////////////////////-->
-
-    function lisa() {
-        const uusToode = {
-            "id": Number(idRef.current.value),
-            "name": nameRef.current.value,
-            "price": Number(priceRef.current.value),
-            "isActive": isActiveRef.current.checked
-        }
-        fetch("https://localhost:7175/tooted/lisa", { "method": "POST", "body": JSON.stringify(uusToode) })
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-    //////////////////////////////////////////////-->
-
-    function dollariteks() {
-        const kurss = 1.1;                                      ////////////////////////
-        fetch("https://localhost:7175/tooted/hind-dollaritesse/" + kurss, { "method": "PATCH" })
-            .then(res => res.json())
-            .then(json => setTooted(json));
-    }
-
-    //////////////////////////////////////////////-->
 
     return (
-        <div className="App">
-            <label>ID</label> <br />
-            <input ref={idRef} type="number" /> <br />
-            <label>Nimi</label> <br />
-            <input ref={nameRef} type="text" /> <br />
-            <label>Hind</label> <br />
-            <input ref={priceRef} type="number" /> <br />
-            <label>Aktiivne</label> <br />
-            <input ref={isActiveRef} type="checkbox" /> <br />
-            <button onClick={() => lisa()}>Lisa</button>
-            {tooted.map((toode, index) =>
-                <div>
-                    <div>{toode.id}</div>
-                    <div>{toode.name}</div>
-                    <div>{toode.price}</div>
-                    <button onClick={() => kustuta(index)}>x</button>
-                </div>)}
-            <button onClick={() => dollariteks()}>Muuda dollariteks</button>
+        <div>
+            <div className="App">
+                <p>Omniva pakiautomaatid</p>
+                <select>
+                    {pakiautomaadid.map(automaat =>
+                        <option>
+                            {automaat.NAME}
+                        </option>)}
+                </select>
+            </div>
         </div>
     );
 }
